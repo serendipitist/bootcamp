523d34a8299eab13fe752713d5de01d213dff503
shireesha <shireesha@quintype.com>
2016-09-26 15:07:40 +0530

    other method added

--- array.rb
+++ array.rb
@@ -1,5 +1,9 @@
 array_elements=[12,3,4,90,23]
 
-for i in array_elements
+for i in array_elements #1st method
   print "value #{i}"
 end
+
+array_elements.each { |x| puts x } #2nd method
+
+array_elements.each_with_index{|val index| puts "#{val} => #{index}"}  #3rd method
